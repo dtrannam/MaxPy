f16dd5a2340983ee7f2bef50b125b9b2334623d1
DavidTrannam <davidtrannam123@yahoo.com>
2021-04-01 00:14:23 -0700

    Added checkbox, graph options, updated layouts

--- Main.py
+++ Main.py
@@ -7,15 +7,15 @@ class mainWindow(qtw.QWidget):
     def __init__(self):
         super().__init__()
         self.setWindowTitle('MaxPr.exe')  # Title
-        self.setLayout(qtw.QVBoxLayout())  # Layout
+        self.setLayout(qtw.QGridLayout())  # Layout
         self.logGUI()
-
+        self.show()
         # Entry fields
 
     def logGUI(self):
         projectLabel = qtw.QLabel('Enter new PR')
-        projectLabel.setFont(QFont('Arial', 15))
-        self.layout().addWidget(projectLabel)
+        projectLabel.setFont(QFont('Arial', 10))
+        self.layout().addWidget(projectLabel, 0, 0)
         # Set Icon
         self.setWindowIcon(QIcon('weights.jpg'))
         # Set window size
@@ -26,30 +26,43 @@ class mainWindow(qtw.QWidget):
         entryLift.addItem('Overhead Press')
         entryLift.addItem('DeadLift')
         entryLift.addItem('Squat')
-        self.layout().addWidget(entryLift)
+        self.layout().addWidget(entryLift, 1, 0)
         # Date Entry
         entryDate = qtw.QLineEdit()
         entryDate.setObjectName('')
         entryDate.setText('[YYYY-MM-DD]')
-        self.layout().addWidget(entryDate)
+        self.layout().addWidget(entryDate, 2, 0)
         # Date Weight
         entryWeight = qtw.QLineEdit()
         entryWeight.setObjectName('')
         entryWeight.setText('[Weight]')
-        self.layout().addWidget(entryWeight)
+        self.layout().addWidget(entryWeight, 3, 0)
         # Date Rep
         entryRep = qtw.QLineEdit()
         entryRep.setObjectName('')
         entryRep.setText('[Rep]')
-        self.layout().addWidget(entryRep)
+        self.layout().addWidget(entryRep, 4, 0)
         # log button
         log_button = qtw.QPushButton('Log Progress', clicked=lambda: add_entry())
         self.layout().addWidget(log_button)
+        # Delete button
+        delete_button = qtw.QPushButton('Delete Item', clicked=lambda: add_entry())
+        self.layout().addWidget(delete_button, 5, 1)
+        # Graph set up
+        checkBP = qtw.QCheckBox('Bench Press')
+        checkOP = qtw.QCheckBox('Overhead Press')
+        checkDL = qtw.QCheckBox('DeadLift')
+        checkST = qtw.QCheckBox('Squat')
+        self.layout().addWidget(checkBP, 1, 2)
+        self.layout().addWidget(checkOP, 2, 2)
+        self.layout().addWidget(checkDL, 3, 2)
+        self.layout().addWidget(checkST, 4, 2)
+        delete_button = qtw.QPushButton('Graph Progress', clicked=lambda: add_entry())
+        self.layout().addWidget(delete_button, 5, 2)
 
-        self.show()
-
-        def add_entry():  # function used to add entry
-            print(f'Pick: {entryLift.currentText()} {entryRep.text()}')
+        # test function
+        def add_entry():
+            print(f'Pick: {entryLift.currentText()} {entryRep.text()} {checkBP.checkState()}')
 
 
 app = qtw.QApplication([])
