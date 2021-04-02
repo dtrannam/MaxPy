a41c64032c5cd4f665ed175acfc55f2ae6115997
DavidTrannam <davidtrannam123@yahoo.com>
2021-04-02 00:07:32 -0700

    Finalized GUI Added DB Needs to adjust log progress

--- Main.py
+++ Main.py
@@ -1,6 +1,8 @@
-# Testing file
 import PyQt5.QtWidgets as qtw
 from PyQt5.QtGui import *
+from datetime import date, datetime
+import sqlite3
+import matplotlib.pyplot as graph
 
 
 class mainWindow(qtw.QWidget):
@@ -8,6 +10,7 @@ class mainWindow(qtw.QWidget):
         super().__init__()
         self.setWindowTitle('MaxPr.exe')  # Title
         self.setLayout(qtw.QGridLayout())  # Layout
+        self.createTable()
         self.logGUI()
         self.show()
         # Entry fields
@@ -43,12 +46,27 @@ class mainWindow(qtw.QWidget):
         entryRep.setText('[Rep]')
         self.layout().addWidget(entryRep, 4, 0)
         # log button
-        log_button = qtw.QPushButton('Log Progress', clicked=lambda: add_entry())
+        log_button = qtw.QPushButton('Log Progress', clicked=lambda: self.logProgress(entryLift.currentText(), entryDate.text(), entryWeight.text(), entryRep.text()))
         self.layout().addWidget(log_button)
         # Delete button
-        delete_button = qtw.QPushButton('Delete Item', clicked=lambda: add_entry())
-        self.layout().addWidget(delete_button, 5, 1)
-        # Graph set up
+        deleteLift = qtw.QComboBox(self)
+        deleteLift.addItem('Bench Press')
+        deleteLift.addItem('Overhead Press')
+        deleteLift.addItem('DeadLift')
+        deleteLift.addItem('Squat')
+        self.layout().addWidget(deleteLift, 1, 1)
+        deleteDate = qtw.QLineEdit()
+        deleteDate.setObjectName('')
+        deleteDate.setText('[YYYY-MM-DD]')
+        self.layout().addWidget(deleteDate, 2, 1)
+        delete_button = qtw.QPushButton('Delete Entry [Input Date]', clicked=lambda: add_entry())
+        self.layout().addWidget(delete_button, 3, 1)
+        # Progress Check
+        show_table = qtw.QPushButton('Display Table', clicked=lambda: add_entry())
+        self.layout().addWidget(show_table, 4, 1)
+        show_graph = qtw.QPushButton('Display Graph', clicked=lambda: add_entry())
+        self.layout().addWidget(show_graph, 5, 1)
+        # CSV set up
         checkBP = qtw.QCheckBox('Bench Press')
         checkOP = qtw.QCheckBox('Overhead Press')
         checkDL = qtw.QCheckBox('DeadLift')
@@ -57,15 +75,39 @@ class mainWindow(qtw.QWidget):
         self.layout().addWidget(checkOP, 2, 2)
         self.layout().addWidget(checkDL, 3, 2)
         self.layout().addWidget(checkST, 4, 2)
-        delete_button = qtw.QPushButton('Graph Progress', clicked=lambda: add_entry())
-        self.layout().addWidget(delete_button, 5, 2)
+        csv_button = qtw.QPushButton('Export to CSV', clicked=lambda: add_entry())
+        self.layout().addWidget(csv_button, 5, 2)
 
         # test function
         def add_entry():
-            print(f'Pick: {entryLift.currentText()} {entryRep.text()} {checkBP.checkState()}')
+            print(f'Pick: {entryDate.text()} {entryLift.currentText()} {entryRep.text()} {checkBP.checkState()}')
+
+    def createTable(self):
+        db = sqlite3.connect("Weight.db")
+        cursor = db.cursor()
+        cursor.execute('''Create Table IF NOT EXISTS WeightPR (
+                        Lift BLOB,
+                        Date BLOB UNIQUE,
+                        InputWeight REAL,
+                        Rep REAL,
+                        EstimatedMax REAL
+                        )''')
+        db.commit()
+        db.close()
+
+    def logProgress(self, lift, day, inputWeight, rep):
+        estimatedMax = inputWeight * (1 + rep / 30)     # using Epley formula
+        db = sqlite3.connect("Weight.db")
+        cursor = db.cursor()
+        cursor.execute('INSERT INTO WeightPR values (?, ?, ?, ?, ?)', (lift, day, inputWeight, rep, estimatedMax))
+        cursor.execute('Select * from WeightPR')    # test - delete later
+        test = cursor.fetchall()                    # test - delete later
+        print(test)                                 # test - delete later
+        db.commit()
+        db.close()
+        pass
 
 
 app = qtw.QApplication([])
 mw = mainWindow()
-
 app.exec_()
