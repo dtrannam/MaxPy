724d05adc1e2273c50dba5cf354bc73c36c18061
DavidTrannam <trannamd@oregonstate.edu>
2021-04-30 00:11:34 -0700

    Refactor code for better readability, adding comments, adjusted table and query to display most recent entries

--- Main.py
+++ Main.py
@@ -8,87 +8,75 @@ import matplotlib.pyplot as graph
 class mainWindow(qtw.QWidget):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle('MaxPr.exe')  # Title
+        # my variable
+        self._lifts = ['Bench Press', 'Overhead Press', 'DeadLift', 'Squat']
+        self._tableHeader = ['Lift', 'Date', 'Input Weight', 'Rep', 'Estimated Max']
+        # GUI
+        self.setWindowTitle('MaxPr')  # Title
         self.setLayout(qtw.QGridLayout())  # Layout
         self.createTable()
+        self.mainGUI()
         self.logGUI()
         self.createTable()
         self.logTable()
         self.show()
-        # Entry fields
 
-    def logTable(self):
-        test = qtw.QTableWidget()
-        test.setRowCount(10)
-        test.setColumnCount(5)
-        test.setItem(0, 0, qtw.QTableWidgetItem("Cell (1,1)"))
-        test.setItem(0, 1, qtw.QTableWidgetItem("Cell (1,2)"))
-        test.setItem(1, 0, qtw.QTableWidgetItem("Cell (2,1)"))
-        test.setItem(1, 1, qtw.QTableWidgetItem("Cell (2,2)"))
-        test.setItem(2, 0, qtw.QTableWidgetItem("Cell (3,1)"))
-        test.setItem(2, 1, qtw.QTableWidgetItem("Cell (3,2)"))
-        test.setItem(3, 0, qtw.QTableWidgetItem("Cell (4,1)"))
-        test.setItem(3, 1, qtw.QTableWidgetItem("Cell (4,2)"))
-        test.horizontalHeader().setStretchLastSection(True)
-        test.horizontalHeader().setSectionResizeMode(qtw.QHeaderView.Stretch)
-        self.layout().addWidget(test, 6, 0, 1, 5)
-
-    def logGUI(self):
+    def mainGUI(self):
+        # handles title/window/sizing
         projectLabel = qtw.QLabel('Enter new PR')
         projectLabel.setFont(QFont('Arial', 10))
         self.layout().addWidget(projectLabel, 0, 0)
-        # Set Icon
         self.setWindowIcon(QIcon('weights.jpg'))
-        # Set window size
         self.setGeometry(500, 500, 500, 500)
-        # Combo Box for which weights
+
+    def logGUI(self):
+        # Combo Box for looping through individual lifts
         entryLift = qtw.QComboBox(self)
-        entryLift.addItem('Bench Press')
-        entryLift.addItem('Overhead Press')
-        entryLift.addItem('DeadLift')
-        entryLift.addItem('Squat')
+        deleteLift = qtw.QComboBox(self)
+        for item in self._lifts:
+            entryLift.addItem(item)
+            deleteLift.addItem(item)
         self.layout().addWidget(entryLift, 1, 0)
-        # Date Entry
+        self.layout().addWidget(deleteLift, 1, 1)
+
+        # Date Entry for adding/deleting items
         entryDate = qtw.QLineEdit()
         entryDate.setObjectName('')
         entryDate.setText('[YYYY-MM-DD]')
         self.layout().addWidget(entryDate, 2, 0)
-        # Date Weight
-        entryWeight = qtw.QLineEdit()
-        entryWeight.setObjectName('')
-        entryWeight.setText('[Weight]')
-        self.layout().addWidget(entryWeight, 3, 0)
-        # Date Rep
-        entryRep = qtw.QLineEdit()
-        entryRep.setObjectName('')
-        entryRep.setText('[Rep]')
-        self.layout().addWidget(entryRep, 4, 0)
-        # log button
-        log_button = qtw.QPushButton('Log Progress', clicked=lambda: self.logProgress(entryLift.currentText(), entryDate.text(), entryWeight.text(), entryRep.text()))
-        self.layout().addWidget(log_button)
-        # Delete button
-        deleteLift = qtw.QComboBox(self)
-        deleteLift.addItem('Bench Press')
-        deleteLift.addItem('Overhead Press')
-        deleteLift.addItem('DeadLift')
-        deleteLift.addItem('Squat')
-        self.layout().addWidget(deleteLift, 1, 1)
         deleteDate = qtw.QLineEdit()
         deleteDate.setObjectName('')
         deleteDate.setText('[YYYY-MM-DD]')
         self.layout().addWidget(deleteDate, 2, 1)
-        delete_button = qtw.QPushButton('Delete Entry [Input Date]', clicked=lambda: self.deleteLog(deleteDate.text(), deleteLift.currentText()))
+
+        # Date Weight/Rep for new weights
+        entryWeight = qtw.QLineEdit()
+        entryWeight.setObjectName('')
+        entryWeight.setText('[Weight]')
+        self.layout().addWidget(entryWeight, 3, 0)
+        entryRep = qtw.QLineEdit()
+        entryRep.setObjectName('')
+        entryRep.setText('[Rep]')
+        self.layout().addWidget(entryRep, 4, 0)
+
+        # Log Button for adding and deleting dates
+        log_button = qtw.QPushButton('Log Progress',
+                                     clicked=lambda: self.logProgress(entryLift.currentText(), entryDate.text(),
+                                                                      entryWeight.text(), entryRep.text()))
+        delete_button = qtw.QPushButton('Delete Entry [Input Date]',
+                                        clicked=lambda: self.deleteLog(deleteDate.text(), deleteLift.currentText()))
+        self.layout().addWidget(log_button, 5, 0)
         self.layout().addWidget(delete_button, 3, 1)
-        # Progress Check
+
+        # Progress Check buttons
         show_table = qtw.QPushButton('Display Table', clicked=lambda: add_entry())
-        self.layout().addWidget(show_table, 4, 1)
         show_graph = qtw.QPushButton('Display Graph', clicked=lambda: self.showGraph(deleteLift.currentText()))
+        self.layout().addWidget(show_table, 4, 1)
         self.layout().addWidget(show_graph, 5, 1)
+
         # CSV set up
-        checkBP = qtw.QCheckBox('Bench Press')
-        checkOP = qtw.QCheckBox('Overhead Press')
-        checkDL = qtw.QCheckBox('DeadLift')
-        checkST = qtw.QCheckBox('Squat')
+        checkBP, checkOP, checkDL, checkST = qtw.QCheckBox(self._lifts[0]), qtw.QCheckBox(
+            self._lifts[1]), qtw.QCheckBox(self._lifts[2]), qtw.QCheckBox(self._lifts[3])
         self.layout().addWidget(checkBP, 1, 2)
         self.layout().addWidget(checkOP, 2, 2)
         self.layout().addWidget(checkDL, 3, 2)
@@ -96,11 +84,44 @@ class mainWindow(qtw.QWidget):
         csv_button = qtw.QPushButton('Export to CSV', clicked=lambda: add_entry())
         self.layout().addWidget(csv_button, 5, 2)
 
-        # test function
-        def add_entry():
+        def add_entry(): # used for button testing - do not move
             print(f'Pick: {entryDate.text()} {entryLift.currentText()} {entryRep.text()} {checkBP.checkState()}')
 
-    # everything here is sqlite queries
+    def logTable(self):
+        # table set up
+        test = qtw.QTableWidget()
+        test.setRowCount(25)
+        test.setColumnCount(5)
+        col = 0
+        row = 0
+
+        # set headers
+        for item in self._tableHeader:
+            test.setItem(row, col, qtw.QTableWidgetItem(item))
+            col += 1
+        col = 0
+        row += 1
+
+        # default query on start
+        db = sqlite3.connect("Weight.db")
+        cursor = db.cursor()
+        cursor.execute('''Select * FROM WeightPR ORDER BY date LIMIT 15''')
+        recentEntry = cursor.fetchall()
+        db.close()
+        for entry in recentEntry:
+            for item in entry:
+                test.setItem(row, col, qtw.QTableWidgetItem(str(item)))
+                col += 1
+            col = 0
+            row += 1
+        col = 0
+        row = 1
+
+        # Table Set up
+        test.horizontalHeader().setStretchLastSection(True)
+        test.horizontalHeader().setSectionResizeMode(qtw.QHeaderView.Stretch)
+        self.layout().addWidget(test, 6, 0, 1, 5)
+
     def createTable(self):
         db = sqlite3.connect("Weight.db")
         cursor = db.cursor()
@@ -115,22 +136,21 @@ class mainWindow(qtw.QWidget):
         db.close()
 
     def logProgress(self, lift, day, inputWeight, rep):
-
         inputWeight = int(inputWeight)
         rep = int(rep)
-        estimatedMax = inputWeight * (1 + rep/ 30)     # using Epley formula
+        estimatedMax = inputWeight * (1 + rep / 30)  # using Epley formula
         db = sqlite3.connect("Weight.db")
         cursor = db.cursor()
-        cursor.execute('INSERT INTO WeightPR values (?, ?, ?, ?, ?) ',(lift, day, inputWeight, rep, estimatedMax))
-        cursor.execute('Select * from WeightPR')    # test - delete later
-        test = cursor.fetchall()                    # test - delete later
-        print(test)                                 # test - delete later
+        cursor.execute('INSERT INTO WeightPR values (?, ?, ?, ?, ?) ', (lift, day, inputWeight, rep, estimatedMax))
+        cursor.execute('Select * from WeightPR')  # test - delete later
+        test = cursor.fetchall()  # test - delete later
+        print(test)  # test - delete later
         db.commit()
         db.close()
         self.testSQL()
 
     def deleteLog(self, day, lift):
-        print(f'deleted {lift} fpr {day}')
+        print(f'deleted {lift} for {day}')
         db = sqlite3.connect("Weight.db")
         cursor = db.cursor()
         cursor.execute(f"Delete From WeightPR where Date = '{day}' AND Lift = '{lift}'")
@@ -139,8 +159,7 @@ class mainWindow(qtw.QWidget):
         self.testSQL()
 
     def showGraph(self, lift):
-        conn = sqlite3.connect('Weight.db')
-        conn.close()
+        # data request and formatting
         dates = []
         numbers = []
         db = sqlite3.connect("Weight.db")
@@ -150,16 +169,17 @@ class mainWindow(qtw.QWidget):
         for item in test:
             dates.append(item[0])
             numbers.append(item[1])
-        print(dates, numbers, lift)
+        print(dates, numbers, lift) # testing function
         convertDate = [datetime.strptime(day, '%Y-%m-%d') for day in dates]
+        db.close()
+
+        # graph display and set up
         graph.plot(convertDate, numbers, label='Progression', linestyle='dashed', color='red')
         graph.legend()
         graph.title(f'{lift}')
         graph.ylabel('Weight')
         graph.xlabel('Date')
         graph.show()
-
-
 
     # testing functions
     def testSQL(self):
@@ -172,6 +192,7 @@ class mainWindow(qtw.QWidget):
 
     def testPython(self):
         print('This is working I hope')
+
 
 app = qtw.QApplication([])
 mw = mainWindow()
